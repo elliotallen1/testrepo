e498d94ec61015a98b05b8226d527b36a129d1e0
elliotallen1 <allened@hendrix.edu>
2023-11-06 12:53:44 -0600

    Commit fr

---
+++ MonstersUniversity copy/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MonstersUniversity.Data;
+using MonstersUniversity.Models;
+
+namespace MonstersUniversity.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MonstersUniversity.Data.SchoolContext _context;
+
+        public IndexModel(MonstersUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Departments != null)
+            {
+                Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+            }
+        }
+    }
+}
